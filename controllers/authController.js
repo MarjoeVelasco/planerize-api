a96078b942c0d268023677700bb086e8c800fcb5
Marjoe Velasco <marjoe.velasco1@gmail.com>
2023-07-17 21:58:53 +0800

    updated auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,7 @@ export const registerUser = asyncHandler( async (req, res) => {
     
     // Generate a JWT token and store in cookies
     const token = jwt.sign({ userId: newUser._id, name: newUser.name }, SECRET_KEY, { expiresIn: '1h' });
-    res.status(200).json({ message: 'User registered successfully', token: token});
+    res.status(200).json({ message: 'User registered successfully, thanks', token: token});
     
   } catch (error) {
     console.error('Failed to register user:', error);
